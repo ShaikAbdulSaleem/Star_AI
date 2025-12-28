3b9453ccd58284b23152fb24fbb89a6a7edde8ed
Shaik Abdul Saleem <163165090+ShaikAbdulSaleem@users.noreply.github.com>
2025-12-28 21:21:05 +0530

    Update Room.js

--- packages/backend/src/models/Room.js
+++ packages/backend/src/models/Room.js
@@ -1,1 +1,14 @@
+  import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+  {
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    match: { type: mongoose.Schema.Types.ObjectId, ref: "Match" },
+    idea: { type: mongoose.Schema.Types.ObjectId, ref: "Idea" },
+    lastMessageAt: Date
+  },
+  { timestamps: true }
+);
+
+export const Room = mongoose.model("Room", roomSchema);
 
