e040d4e9d885c7cb8138d07a36169240bb5782de
Shaik Abdul Saleem <163165090+ShaikAbdulSaleem@users.noreply.github.com>
2025-12-28 21:50:15 +0530

    Implement getRiskTrendForIdea controller function

--- packages/backend/src/controllers/insight.controller.js
+++ packages/backend/src/controllers/insight.controller.js
@@ -1,1 +1,34 @@
+// packages/backend/src/controllers/insight.controller.js
+import { RiskScore } from "../models/RiskScore.js";
+import { Idea } from "../models/Idea.js";
+
+export const getRiskTrendForIdea = async (req, res, next) => {
+  try {
+    const { ideaId } = req.params;
+
+    const idea = await Idea.findOne({ _id: ideaId, owner: req.user._id });
+    if (!idea) {
+      return res.status(404).json({ message: "Idea not found" });
+    }
+
+    const scores = await RiskScore.find({ idea: ideaId }).sort({ snapshotAt: 1 });
+
+    if (!scores.length) {
+      return res.json({ improvementPercent: 0, scores: [] });
+    }
+
+    const first = scores[0].scores.overall || 0;
+    const last = scores[scores.length - 1].scores.overall || 0;
+
+    const improvementPercent =
+      first === 0 ? 0 : Math.round(((first - last) / first) * 100);
+
+    res.json({
+      improvementPercent,
+      scores
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
