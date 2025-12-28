1f2917846bbbf3f8770cb3656974f71c54d6a2d6
Shaik Abdul Saleem <163165090+ShaikAbdulSaleem@users.noreply.github.com>
2025-12-28 21:21:42 +0530

    Update ChatMessage.js

--- packages/backend/src/models/ChatMessage.js
+++ packages/backend/src/models/ChatMessage.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
+
+const chatMessageSchema = new mongoose.Schema(
+  {
+    room: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+  },
+  { timestamps: true }
+);
+
+export const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
 
